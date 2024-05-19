770178008b4a9157c33cf37200907a5add38cb67
Chandan Kumar <nightfury@Chandans-MacBook-Air.local>
2024-05-19 17:29:20 +0530

    init

---
+++ Grpc-service/src/main/java/com/example/mongo/grpc/HelloWorldGrpcImpl.java
@@ -0,0 +1,21 @@
+package com.example.mongo.grpc;
+
+import com.example.mongo.stubs.HelloWorldRequest;
+import com.example.mongo.stubs.HelloWorldResponse;
+import com.example.mongo.stubs.HelloWorldServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloWorldGrpcImpl extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+
+    @Override
+    public void sayHello(HelloWorldRequest request, StreamObserver<HelloWorldResponse> responseObserver) {
+//        super.sayHello(request, responseObserver);
+        System.out.println("request: " + request);
+        responseObserver.onNext(HelloWorldResponse.newBuilder()
+                .setGreeting("Greeting")
+                .build());
+        responseObserver.onCompleted();
+    }
+}
